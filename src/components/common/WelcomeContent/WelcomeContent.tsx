a6388d9bc1ace1d708dce9183a7a8f236d718f84
Jon Ryser <241263+jonryser@users.noreply.github.com>
2023-05-17 11:53:34 -0700

    Fixed WelcomeContent to handle no button.

--- src/components/common/WelcomeContent/WelcomeContent.tsx
+++ src/components/common/WelcomeContent/WelcomeContent.tsx
@@ -5,6 +5,11 @@ import { useText } from 'hooks/useText'
 import { Button } from 'common/Button'
 import { WelcomeContentProps } from './WelcomeContent.types'
 
+interface AboutConfig {
+	image: string
+	link?: string
+}
+
 export const WelcomeContent = ({
 	className,
 	description,
@@ -12,27 +17,31 @@ export const WelcomeContent = ({
 	testId = 'WelcomeContent',
 	title
 }: WelcomeContentProps) => {
-	const { image, link } = about
+	const { image, link } = about as AboutConfig
 	const { t } = useText('client.about')
 
 	return (
 		<div data-testid={testId} className={cn('block w-full xl:w-9/12', className)}>
-			<div className='flex flex-col lg:flex-row gap-4 lg:gap-5 justify-between'>
+			<div className={'flex flex-col lg:flex-row gap-4 lg:gap-5 justify-between'}>
 				{!isParticipant && (
-					<figure className='relative w-36 md:w-40 lg:w-48 xl:w-52 h-36 md:h-40 lg:h-48 xl:h-52 flex-shrink-0'>
-						<Image className='rounded-xl' layout='fill' src={image} alt={t('imageAlt')} />
+					<figure
+						className={
+							'relative w-36 md:w-40 lg:w-48 xl:w-52 h-36 md:h-40 lg:h-48 xl:h-52 flex-shrink-0'
+						}
+					>
+						<Image className={'rounded-xl'} fill src={image} alt={t('imageAlt')} />
 					</figure>
 				)}
-
-				<div className='flex flex-col gap-4 lg:gap-2 xl:gap-3 justify-space-between'>
-					<h2 className='font-bold text-3xl'>{title}</h2>
-					<p>{description}</p>
-
-					<div className='block'>
-						<Button link href={link} className='w-max'>
-							{t('linkLabel')}
-						</Button>
-					</div>
+				<div className={'flex flex-col gap-4 lg:gap-2 xl:gap-3 justify-space-between'}>
+					<h2 className={'font-bold text-3xl'}>{title}</h2>
+					<p className={'whitespace-pre-wrap'}>{description}</p>
+					{link && (
+						<div className={'block'}>
+							<Button link href={link} className={'w-max'}>
+								{t('linkLabel')}
+							</Button>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
